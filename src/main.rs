e9330e4c9fcd086411c6b16b6076a4e85276c854
Mike <antonovma@outlook.com>
2022-09-01 17:38:49 +0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use std::collections::HashMap;
 mod structures;
 
 fn main() {
+    let file = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open("config.yml")
+        .expect("Couldn't open file");
+
     let mut setings = HashMap::new();
 
     for i in 0..4 {
@@ -21,37 +27,36 @@ fn main() {
     for (key, value) in &setings {
         println!("{}: \n{}", key, value);
     }
+
+    serde_yaml::to_writer(file, &setings).unwrap();
 }
 
 fn environment_data(iroha_iter: &String) -> Environment {
     let envir = Environment {
-        TORII_P2P_ADDR: format!("{}:1337", iroha_iter),
-        TORII_API_URL: format!("{}:8080", iroha_iter),
+        TORII_P2P_ADDR:      format!("{}:1337", iroha_iter),
+        TORII_API_URL:       format!("{}:8080", iroha_iter),
         TORII_TELEMETRY_URL: format!("{}:8180", iroha_iter),
-        IROHA_PUBLIC_KEY: "EMPTY".to_string(),
-        IROHA_PRIVATE_KEY: "EMPTY".to_string(),
-        SUMERAGI_TRUSTED_PEERS: "EMPTY".to_string(),
+        IROHA_PUBLIC_KEY:       dummy(),
+        IROHA_PRIVATE_KEY:      dummy(),
+        SUMERAGI_TRUSTED_PEERS: dummy(),
     };
     envir
 }
 
+fn dummy() -> String { "EMPTY".to_string() }
+
 fn value_maker(iroha_iter: &String) -> Vec<u8> {
-    let entry = IrohaIterated {
+    let irohaiter = IrohaIterated {
         build: '.',
         image: "iroha2:dev".to_string(),
-        volumes: "
-        - './configs/peer:/config'
-        - './:/root/soramitsu/iroha'".to_string(),
+        volumes: "- './configs/peer:/config'\n- './:/root/soramitsu/iroha'".to_string(),
         environment: environment_data(&iroha_iter),
-        ports: "
-        - \"1337:1337\"
-        - \"8080:8080\"
-        - \"8180:8180\"".to_string(),
+        ports: "- \"1337:1337\"\n- \"8080:8080\"\n- \"8180:8180\"".to_string(),
         init: true,
         command: "iroha --submit-genesis".to_string(),
     };
 
-    let serialized = serde_yaml::to_string(&entry)
+    let serialized = serde_yaml::to_string(&irohaiter)
         .unwrap()
         .clone()
         .into_bytes();
